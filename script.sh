2654c0b58774b7d2bb63eb0195daf5fe2c94abfb
TheMarkedOne <132334856+TheMarkedOne@users.noreply.github.com>
2023-05-06 18:54:42 +0400

    Delete script.sh

--- script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "# DjangoBehindNginx" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://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@github.com/TheMarkedOne/DjangoBehindNginx.git
-git push -u origin main
